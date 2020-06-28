3eefe6a583d97d0ec06e6e2acecd144facf766b9
PabloGonzalezM <pablogonzalezmolina@hotmail.es>
2020-06-28 19:36:52 +0200

    Core(golang): Add main.go which will allow to run each one of the katas or tests

---
+++ golang/katas/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    menu()
+}
+
+func menu() {
+    fmt.Printf("User Menu:")
+    fmt.Printf("0 - Exit.")
+    fmt.Printf("1 - Hello.")
+    fmt.Printf("2 - Bye.")
+    fmt.Printf("3 - IDK.")
+}
